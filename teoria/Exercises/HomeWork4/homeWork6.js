1a4a45dd85f7086668c84f8b55ceba25c5e3888e
Michał Walewski <pelikan2333@proton.me>
2025-02-09 12:18:19 +0100

    add changes files

--- teoria/Exercises/HomeWork4/homeWork6.js
+++ teoria/Exercises/HomeWork4/homeWork6.js
@@ -3,4 +3,7 @@ const showMessage = function (message) {
 }
  showMessage("Hello, World!");  // wywołanie funkcji showMessage z argumentem "Hello, World!"
 
- 
+const showMeTextMyArrowFunction = (text) => {
+    console.log(text);
+}
+ showMeTextMyArrowFunction("hej"); // wywołanie funkcji showMeTextMyArrowFunction z argumentem "hej"
